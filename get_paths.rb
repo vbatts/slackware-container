c9e868399ac96de4fffd23379ff59b95791e4ca7
Vincent Batts <vbatts@redhat.com>
2013-11-27 16:24:35 -0500

    now the ruby helper prints required packages from all sections

--- get_paths.rb
+++ get_paths.rb
@@ -1,14 +1,90 @@
+#!/usr/bin/env ruby
+
 require 'open-uri'
 
-@base_mirror = "http://mirrors1.kernel.org/slackware"
+module Slackware
+  class Repo
+    DEFAULT_MIRROR = "http://mirrors1.kernel.org/slackware"
+    DEFAULT_RELEASE = "slackware64-current"
+    RE_FILELIST = /.*(\d{4}-\d{2}-\d{2} \d{2}:\d{2})\s\.\/(\w*)\/(.*\.t.z)\n?/
 
-fl = open(@base_mirror + "/slackware64-current/slackware64/FILE_LIST")
-re = Regexp.new(".*/a/\(.*\.t.z\)\n")
-pkgs = { :a => [] }
-fl.each_line do |line|
-  if line =~ re 
-    pkgs[:a] << $1
+    def initialize(release = nil, mirror = nil)
+      @mirror = mirror || DEFAULT_MIRROR
+      @release = release || DEFAULT_RELEASE
+      @tag_done = false
+      @pkgs = []
+    end
+
+    def pkgs
+      return @pkgs unless @pkgs.empty?
+      fl = open("#{@mirror}/#{@release}/#{_base_rel}/FILE_LIST")
+      fl.each_line do |line|
+        line.strip!
+        if line =~ RE_FILELIST 
+          @pkgs << Slackware::Package.parse($2 + "/" + $3)
+        end
+      end
+      fl.close
+      return @pkgs
+    end
+
+    def sections
+      return pkgs.map {|p| p.section }.uniq
+    end
+
+    def tagfiles
+      return pkgs.map {|p| p.tag }.uniq if @tag_done
+      sections.each do |section|
+        fl = open("#{@mirror}/#{@release}/#{_base_rel}/#{section}/tagfile")
+        fl.each_line do |line|
+          p_name, tag = line.strip.split(":",2)
+          pkg = pkgs.detect {|p| p.name == p_name }
+          if pkg
+            pkg.tag = tag 
+          else
+            $stderr.puts("#{p_name} not in pkgs")
+          end
+        end
+        fl.close
+      end
+      @tag_done = true
+      return @pkgs.map {|p| p.tag }.uniq
+    end
+
+    private
+    def _base_rel
+      @release.split("-").first
+    end
+  end
+
+  class Package
+    attr_accessor :section, :name, :version, :arch, :build, :ext, :tag
+    def self.parse(str)
+      pkg = self.new
+      paths = str.split('/')
+      pkg_str = paths.pop
+      if paths.length > 0
+        pkg.section = paths.join('/')
+      end
+      pkg.ext = File.extname(pkg_str)
+      chunks = File.basename(pkg_str, pkg.ext).split('-')
+      pkg.build = chunks.pop
+      pkg.arch = chunks.pop
+      pkg.version = chunks.pop
+      pkg.name = chunks.join('-')
+      return pkg
+    end
+    def to_s
+      "#{@section.nil? ? '' : @section + '/'}#{@name}-#{@version}-#{@arch}-#{@build}#{@ext}"
+    end
   end
 end
 
-puts pkgs
+# build out the tag files
+r = Slackware::Repo.new
+r.tagfiles
+r.pkgs.each do |pkg|
+  puts pkg if pkg.tag == "REC"
+end
+
+
