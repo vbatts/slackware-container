ceffd44457149ca5b1cda8e3124f2c5b376b2e94
Vincent Batts <vbatts@hashbangbash.com>
2013-12-08 10:17:43 -0500

    adding an httpd build

--- mkimage-slackware.sh
+++ mkimage-slackware.sh
@@ -50,6 +50,22 @@ cp etc/ld.so.conf mnt/etc
 
 relbase=$(echo ${RELEASE} | cut -d- -f1)
 #for pkg in $(curl -s ${MIRROR}/${RELEASE}/${RELBASE}/a/tagfile | grep REC$ | cut -d : -f 1)
+	#a/inotify-tools-3.14-x86_64-1.txz \
+	#a/lvm2-2.02.100-x86_64-1.txz \
+	#l/glibc-2.17-x86_64-7.txz \
+	#ap/ksh93-2012_08_01-x86_64-1.txz \
+	#a/btrfs-progs-20130418-x86_64-1.txz \
+	#a/dosfstools-3.0.22-x86_64-1.txz \
+	#a/ed-1.9-x86_64-1.txz \
+	#a/lha-114i-x86_64-1.txz \
+	#a/udisks-1.0.4-x86_64-2.txz \
+	#a/udisks2-2.1.0-x86_64-1.txz \
+	#a/dbus-1.6.12-x86_64-1.txz \
+	#a/ncompress-4.2.4.3-x86_64-1.txz \
+	#a/mtx-1.3.12-x86_64-1.txz \
+	#a/mcelog-1.0pre3-x86_64-1.txz \
+	#a/unarj-265-x86_64-1.txz \
+	#a/tcsh-6.18.01-x86_64-2.txz \
 for pkg in \
 	a/aaa_base-14.1-x86_64-1.txz \
 	a/aaa_elflibs-14.1-x86_64-3.txz \
@@ -60,8 +76,6 @@ for pkg in \
 	a/tar-1.26-x86_64-1.tgz \
 	a/xz-5.0.5-x86_64-1.tgz \
 	a/bash-4.2.045-x86_64-1.txz \
-	ap/ksh93-2012_08_01-x86_64-1.txz \
-	l/glibc-2.17-x86_64-7.txz \
 	a/etc-14.1-x86_64-2.txz \
 	a/gzip-1.6-x86_64-1.txz \
 	n/wget-1.14-x86_64-2.txz \
@@ -73,30 +87,16 @@ for pkg in \
 	a/bzip2-1.0.6-x86_64-1.txz \
 	a/grep-2.14-x86_64-1.txz \
 	a/sed-4.2.2-x86_64-1.txz \
-	a/btrfs-progs-20130418-x86_64-1.txz \
-	a/dbus-1.6.12-x86_64-1.txz \
 	a/dialog-1.2_20130523-x86_64-1.txz \
-	a/dosfstools-3.0.22-x86_64-1.txz \
-	a/ed-1.9-x86_64-1.txz \
 	a/file-5.14-x86_64-1.txz \
 	a/gawk-4.1.0-x86_64-2.txz \
 	a/time-1.7-x86_64-1.txz \
 	a/gettext-0.18.2.1-x86_64-2.txz \
-	a/inotify-tools-3.14-x86_64-1.txz \
-	a/lha-114i-x86_64-1.txz \
 	a/libcgroup-0.38-x86_64-2.txz \
-	a/lvm2-2.02.100-x86_64-1.txz \
-	a/mcelog-1.0pre3-x86_64-1.txz \
-	a/mtx-1.3.12-x86_64-1.txz \
-	a/ncompress-4.2.4.3-x86_64-1.txz \
 	a/patch-2.7-x86_64-2.txz \
 	a/sysfsutils-2.1.0-x86_64-1.txz \
-	a/tcsh-6.18.01-x86_64-2.txz \
 	a/time-1.7-x86_64-1.txz \
 	a/tree-1.6.0-x86_64-1.txz \
-	a/udisks-1.0.4-x86_64-2.txz \
-	a/udisks2-2.1.0-x86_64-1.txz \
-	a/unarj-265-x86_64-1.txz \
 	a/utempter-1.1.5-x86_64-1.txz \
 	a/which-2.20-x86_64-1.txz \
 	a/util-linux-2.21.2-x86_64-6.txz \
@@ -112,6 +112,9 @@ done
 
 cd mnt
 touch etc/resolv.conf
+echo "export TERM=linux" >> etc/profile.d/term.sh
+chmod +x etc/profile.d/term.sh
+echo ". /etc/profile" > .bashrc
 echo "${MIRROR}/${RELEASE}/" >> etc/slackpkg/mirrors
 sed -i 's/DIALOG=on/DIALOG=off/' etc/slackpkg/slackpkg.conf
 
